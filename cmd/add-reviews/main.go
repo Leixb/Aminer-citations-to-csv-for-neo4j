eef909d92a982ba9e23b611a50f3afaa6f957493
LeixB <abone9999@gmail.com>
2022-03-25 16:22:00 +0100

    Fix wrong reviews output filename

--- cmd/add-reviews/main.go
+++ cmd/add-reviews/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
     var input, output string
     flag.StringVar(&input, "input file", "data/rel_authored.csv", "File containing the article -> author relation in csv format")
-    flag.StringVar(&output, "output file", "data/rel_authored.csv", "Output file where the review relation will be saved")
+    flag.StringVar(&output, "output file", "data/rel_reviews.csv", "Output file where the review relation will be saved")
     flag.Parse()
 
     f, err := os.Open(input)
